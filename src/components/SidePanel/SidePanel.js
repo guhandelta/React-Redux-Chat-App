18abfa6c08a82fcb35b09b8e0edec52c053e91d8
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2020-06-12 00:00:23 -0400

    Constructed teh basic structure of the chat app

---
+++ src/components/SidePanel/SidePanel.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class SidePanel extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Side Panel</h1>
+            </div>
+        )
+    }
+}
+
+export default SidePanel;
