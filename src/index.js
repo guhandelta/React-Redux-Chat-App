836b82bf476c86dd5e5bc9a4ddc73a5c2a6e9e1b
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2020-06-11 23:45:56 -0400

    Added a spinner component to the Loading state

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { Login, Register } from './components/Auth'
 import * as serviceWorker from './serviceWorker';
 import firebase from './firebase'
 import { setUser } from './actions'
+import Spinner from './Spinner'
 
 import { createStore } from 'redux'
 import { Provider, connect } from 'react-redux'
@@ -18,6 +19,7 @@ const store = createStore(rootreducer, composeWithDevTools())
 class Root extends React.Component {
 
   componentDidMount() {
+    console.log(this.props.isLoading);
     // A listener to detect whether a user is available in the app
     firebase.auth().onAuthStateChanged(user => {
       if (user) {
@@ -29,7 +31,7 @@ class Root extends React.Component {
   }
 
   render() {
-    return (
+    return this.props.isLoading ? <Spinner /> : (
       <Switch>
         <Route exact path="/" component={App} />
         <Route path="/Login" component={Login} />
@@ -39,11 +41,17 @@ class Root extends React.Component {
   }
 }
 
+const mapStateToProps = state => ({
+  isLoading: state.user.isLoading
+  // user is the property of the state holding the root_reducer obtained from combineReducers(), reducers/index.js
+})
+
 // Wrap the Root component with the withROuter HOC, to put the history object within the Root component
-const RootWithAuth = withRouter(connect(null, { setUser })(Root));
+const RootWithAuth = withRouter(connect(mapStateToProps, { setUser })(Root));
 // connect(mapStateToProps, mapDispatchStateToProps{where the actions are received from})
 // connect() and mapDispatchToProps will take the setUser action and put it on the props obj of component, which is wrapped with connect()
-
+// mapStateToProps is passed as teh 1st arg to connect() to get the loading data from the state object, to see when the setUser action-
+//- is still being loaded
 
 ReactDOM.render(
   <Provider store={store}>
