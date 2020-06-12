997e3dbe4b1edefd2d1f9b298804b287887a2869
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2020-06-11 23:25:46 -0400

    Setup Redux to make the User Data Available on Global State

--- src/index.js
+++ src/index.js
@@ -6,6 +6,14 @@ import App from './components/App';
 import { Login, Register } from './components/Auth'
 import * as serviceWorker from './serviceWorker';
 import firebase from './firebase'
+import { setUser } from './actions'
+
+import { createStore } from 'redux'
+import { Provider, connect } from 'react-redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import rootreducer from './reducers';
+
+const store = createStore(rootreducer, composeWithDevTools())
 
 class Root extends React.Component {
 
@@ -13,7 +21,9 @@ class Root extends React.Component {
     // A listener to detect whether a user is available in the app
     firebase.auth().onAuthStateChanged(user => {
       if (user) {
-        this.props.history.push('/')
+        // console.log(user);
+        this.props.setUser(user);
+        this.props.history.push('/');
       }
     })
   }
@@ -30,12 +40,17 @@ class Root extends React.Component {
 }
 
 // Wrap the Root component with the withROuter HOC, to put the history object within the Root component
-const RootWithAuth = withRouter(Root);
+const RootWithAuth = withRouter(connect(null, { setUser })(Root));
+// connect(mapStateToProps, mapDispatchStateToProps{where the actions are received from})
+// connect() and mapDispatchToProps will take the setUser action and put it on the props obj of component, which is wrapped with connect()
+
 
 ReactDOM.render(
-  <Router>
-    <RootWithAuth />
-  </Router>, document.getElementById("root"));
+  <Provider store={store}>
+    <Router>
+      <RootWithAuth />
+    </Router>
+  </ Provider>, document.getElementById("root"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
