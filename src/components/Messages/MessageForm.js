d1e4b3f138e3a651509dff8a06aa4fe70c9a02a8
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2020-06-13 23:40:10 -0400

    Converted the input to a controlled input, to clear it after sending the message and Disabled the send button when loading: true

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -56,7 +56,7 @@ class MessageForm extends Component {
     }
 
     render() {
-        const { errors } = this.state;
+        const { errors, message, loading } = this.state;
         return (
             <Segment
                 clasName="message__form"
@@ -64,6 +64,7 @@ class MessageForm extends Component {
                 <Input
                     fluid
                     name="message"
+                    value={message}
                     onChange={this.handleChange}
                     style={{ marginBottom: '0.7em' }}
                     label={<Button icon={'add'} />}
@@ -72,10 +73,11 @@ class MessageForm extends Component {
                         errors.some(error => error.message.includes("message")) ? "error" : ""
                     }
                     placeholder="Write your Message"
-                />
+                />{/* Button while be disabled while loading is true || while msg is being added*/}
                 <Button.Group icon widths="2">
                     <Button
                         onClick={this.sendMessage}
+                        disabled={loading}
                         color="orange"
                         content="Add Reply"
                         labelPosition="left"
