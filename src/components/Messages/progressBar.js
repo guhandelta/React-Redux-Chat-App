cd3eccfdff32cde6f6924c0a8c4b645fb35884d6
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2020-06-14 17:58:34 -0400

    Provided appropriate styling changes for the  ProgressBar component to disappear, after the image upload

--- src/components/Messages/progressBar.js
+++ src/components/Messages/progressBar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Progress } from 'semantic-ui-react'
 
 const progressBar = ({ uploadState, percentUploaded }) => (
-    uploadState && (
+    uploadState === 'uploading' && (
         <Progress
             className="progress__bar"
             percent={percentUploaded}
